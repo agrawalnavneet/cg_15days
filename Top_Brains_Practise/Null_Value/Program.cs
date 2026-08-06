240fff0916a44bbd6f323bf67b0fabbf17c5f752
Navneet Agarwal <agrawalnavneet960@gmail.com>
2026-08-06 17:32:20 +0530

    added

---
+++ Top_Brains_Practise/Null_Value/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Solution
+{
+    public static double? AverageNonNull(double?[] values)
+    {
+        double sum = 0;
+        int count = 0;
+
+        foreach (double? value in values)
+        {
+            if (value.HasValue)
+            {
+                sum += value.Value;
+                count++;
+            }
+        }
+
+        if (count == 0)
+            return null;
+
+        return Math.Round(sum / count, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main()
+    {
+        double?[] values = { 10.5, null, 20.5, 30.0, null };
+
+        double? result = AverageNonNull(values);
+
+        Console.WriteLine(result.HasValue ? result.Value.ToString("F2") : "null");
+    }
+}
