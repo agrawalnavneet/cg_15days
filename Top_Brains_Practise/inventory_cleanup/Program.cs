289eba5f82964058de1a283b9bfe8b04e63a76c4
Navneet Agarwal <agrawalnavneet960@gmail.com>
2026-08-12 20:00:24 +0530

    added

---
+++ Top_Brains_Practise/inventory_cleanup/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        string input = " llapppptop bag ";
+        input = input.Trim();
+
+        string result = "";
+
+        foreach (char c in input)
+        {
+            if (result.Length == 0 || result[result.Length - 1] != c)
+            {
+                result += c;
+            }
+        }
+
+        result = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(result.ToLower());
+
+        Console.WriteLine(result);
+    }
+}
